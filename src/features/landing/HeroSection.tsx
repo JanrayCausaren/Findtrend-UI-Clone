13fc72adc9a10040dde66f7f7290ad5deb3ec825
Janray Causaren <3ray.causaren@gmail.com>
2026-03-18 23:13:09 +0800

    chore: remove boilerplate

---
+++ src/features/landing/HeroSection.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+type Props = {};
+
+const HeroSection = (props: Props) => {
+  return (
+    <>
+      <section className="hero__section">
+        <div className="container mx-auto">
+          <h1>Minimize your tabs.</h1>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default HeroSection;
